452da6f369ccf812ed2213bb0463e3bfd78d1c40
Furkan Tezeren <emintezeren@gmail.com>
2020-04-02 16:34:48 +0300

    Fixed for mobile

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import '../styles/Loader.css';
 
-export const Loader = () => {
-  return (
+export const Loader = () => (
     <div className="stats">
       <div className="loader">
       </div>
     </div>
-  )
-}
+)
