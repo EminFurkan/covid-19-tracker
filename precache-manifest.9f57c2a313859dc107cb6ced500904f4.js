8a7079b07beef89dc3385e0ec769581723e99c64
Furkan Tezeren <emintezeren@gmail.com>
2020-04-09 12:38:10 +0300

    Updates

---
+++ precache-manifest.9f57c2a313859dc107cb6ced500904f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f59983bdfdcbcf6a962def0188b7ce",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "26c106464382eec63e56",
+    "url": "/covid-19-tracker/static/css/main.e75ff875.chunk.css"
+  },
+  {
+    "revision": "9048ae7095745a53816d",
+    "url": "/covid-19-tracker/static/js/2.0623ad30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-tracker/static/js/2.0623ad30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c106464382eec63e56",
+    "url": "/covid-19-tracker/static/js/main.fe2d89e8.chunk.js"
+  },
+  {
+    "revision": "c7ee893204412e748ec1",
+    "url": "/covid-19-tracker/static/js/runtime-main.72171ba5.js"
+  }
+]);
